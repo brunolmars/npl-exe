c9a27cc6f5cd2ba375b07ab49a93fc53cd5602f5
brunolmars <bruno.lmarques@sempreceub.com>
2024-09-02 19:59:18 -0300

    s

--- tokenização.py
+++ tokenização.py
@@ -1,12 +1,17 @@
-import nltk
+from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.corpus import stopwords
+from nltk.collocations import *
+from string import punctuation
+from nltk.probability import FreqDist
+from heapq import nlargest
+from collections import defaultdict
 
-def tokenizacao():
-    txt = """France, in Western Europe, encompasses medieval cities, alpine villages and Mediterranean beaches. Paris, its capital, is famed for its fashion houses, classical art museums including the Louvre and monuments like the Eiffel Tower. The country is also renowned for its wines and sophisticated cuisine. Lascaux’s ancient cave drawings, Lyon’s Roman theater and the vast Palace of Versailles attest to its rich history."""
-    
-    tokens = nltk.word_tokenize(txt)
-    print(tokens)
 
+def tokenization(n, txt):
 
+    text =  """France, in Western Europe, encompasses medieval cities, alpine villages and Mediterranean beaches. Paris, its capital, is famed for its fashion houses, classical art museums including the Louvre and monuments like the Eiffel Tower. The country is also renowned for its wines and sophisticated cuisine. Lascaux’s ancient cave drawings, Lyon’s Roman theater and the vast Palace of Versailles attest to its rich history."""
+    token = word_tokenize(text)
 
-def main():
-    tokenizacao()
+    assert n <= len(token)
+    sent = word_tokenize(text.lower()) 
+
