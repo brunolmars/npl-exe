cb4c2f04652d6182c6454e70cbb5c442f0857ea7
brunolmars <bruno.lmarques@sempreceub.com>
2024-09-04 19:11:42 -0300

    s

--- tokenização.py
+++ tokenização.py
@@ -1,25 +1,39 @@
-from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
-from nltk.collocations import *
 from string import punctuation
-from nltk.probability import FreqDist
-from heapq import nlargest
 from collections import defaultdict
+import pandas as pd
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
 
-ranking = defaultdict
-
+ranking = defaultdict(int)
 
 def tokenization(n):
-
-    text =  """France, in Western Europe, encompasses medieval cities, alpine villages and Mediterranean beaches. Paris, its capital, is famed for its fashion houses, classical art museums including the Louvre and monuments like the Eiffel Tower. The country is also renowned for its wines and sophisticated cuisine. Lascaux’s ancient cave drawings, Lyon’s Roman theater and the vast Palace of Versailles attest to its rich history."""
-    token = word_tokenize(text)
-
-    assert n <= len(token)
     
-    sent = [token.lower() for token in token]
+    text_data = pd.read_csv("text.txt", delimiter=";", names=["text", "label"])
+    token_list = []
+    
+    for text in text_data["text"]:
+        tokens = word_tokenize(text)
+        token_list.extend(tokens)
+        
+        assert n <= len(token_list), "O valor de n é maior que o número de tokens disponíveis."
+    
+        tokens_lower = [token.lower() for token in token_list]
+    
     stop_words = set(stopwords.words('english') + list(punctuation))
-
-    filtoken =[token for token in sent if token not in stop_words]
-
     
+    filtered_tokens = [token for token in tokens_lower if token not in stop_words]
+
+    plt.rcParams['figure.figsize'] = (20, 8)
+        
+    word_cloud = " ".join(filtered_tokens)
+    wordcloud = WordCloud(width=1000, height=500, background_color='white', min_font_size=10).generate(word_cloud)
+    
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis('off')
+    plt.show()
+
+
+tokenization(100)
 
