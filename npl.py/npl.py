1701f504c177b8fd59ea466c58b55f0eae7c6683
brunolmars <bruno.lmarques@sempreceub.com>
2024-09-09 20:59:00 -0300

    s

--- npl.py/npl.py
+++ npl.py/npl.py
@@ -2,39 +2,47 @@ from gensim.models import Word2Vec
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
-from nltk.stem import PorterStemmer
+from nltk.wsd import lesk
 from string import punctuation
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
 import pandas as pd
+import spacy
 
+text_data = pd.read_csv("text.txt", delimiter=";", names=["text"])
+token_list = []
 
-def tokenization():
-    
-    text_data = pd.read_csv("text.txt", delimiter=";", names=["text", "label"])
-    token_list = []
-    
+def text_trasformeted(text_data):    
     for text in text_data["text"]:
-        tokens = word_tokenize(text)
-        tokens_lower = [token.lower() for token in tokens]
+            
+            tokens = word_tokenize(text)
+            tokens_lower = [token.lower() for token in tokens]
+            stop_words = set(stopwords.words('english') + list(punctuation))
+            filtered_tokens = [token for token in tokens_lower if token not in stop_words]
+            lemetizer = WordNetLemmatizer()
+            token_lemmatizer = [lemetizer.lemmatize(token) for token in filtered_tokens ]
+            token_list.append(" ".join(token_lemmatizer))
     
-        stop_words = set(stopwords.words('english') + list(punctuation))
-    
-        filtered_tokens = [token for token in tokens_lower if token not in stop_words]
+    return token_list
 
-        lemetizer = WordNetLemmatizer()
-        token_lemmatizer = [lemetizer.lemmatize(token) for token in filtered_tokens ]
+token_list = text_trasformeted(text_data)
 
-        stemming = PorterStemmer()
-        token_stemming = [stemming.stem(token) for token in token_lemmatizer]
+coutvectorize = CountVectorizer()
+token_vector = coutvectorize.fit_transform(token_list)
 
-        token_list.append(" ".join(token_stemming))
+w2v = Word2Vec(sentences= token_list, min_count= 1)
+token_word = w2v
 
-    vectorize = TfidfVectorizer()
-    token_vector = vectorize.fit_transform(token_list)
+token_sent = lesk(token_list[0],"paris")
 
-    model = Word2Vec(sentences=token_list,min_count = 1)
-    word = list(model.wv.index_to_key)
-        
+nlp = spacy.load("en_core_web_sm")
+token_sent2 =nlp("".join(token_list[0]))
+for token in token_sent2:
+    print("{0}".format(token.head.text))
+
+
+
+
+
 
 
 
