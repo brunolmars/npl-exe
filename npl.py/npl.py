de7ea76be31b2fdc0dec16146abc4e550fa1c2b9
brunolmars <bruno.lmarques@sempreceub.com>
2024-09-30 19:03:03 -0300

    s

--- npl.py/npl.py
+++ npl.py/npl.py
@@ -10,12 +10,12 @@ from sklearn.feature_extraction.text import CountVectorizer
 import pandas as pd
 import spacy
 
-text_data = pd.read_csv("text.txt", delimiter=";", names=["text"])
+token_data = pd.read_csv("text.txt", delimiter=";", names=["text"])
 token_list = []
 
-def text_trasformeted(text_data):    
+def text_trasformeted(text_data):
+    global token_list
     for text in text_data["text"]:
-            
             tokens = word_tokenize(text)
             tokens_lower = [token.lower() for token in tokens]
             stop_words = set(stopwords.words('english') + list(punctuation))
@@ -23,26 +23,41 @@ def text_trasformeted(text_data):
             lemetizer = WordNetLemmatizer()
             token_lemmatizer = [lemetizer.lemmatize(token) for token in filtered_tokens ]
             token_list.append(" ".join(token_lemmatizer))
-    
+
     return token_list
 
-token_list = text_trasformeted(text_data)
+
+def palavras_enumeradas(token_list):
+
+    token_list = [token.replace("surprise", "1")
+                  .replace("happy", "1")
+                  .replace("sad", "0")
+                  .replace("angry", "0")
+                  .replace("love", "1")
+                  .replace("fear", "0")
+                  .replace("joy", "1")
+                  .replace("sadness", "0") for token in token_list]
+
+    return token_list
+
+
+
+token_process = text_trasformeted(token_list)
+print(token_list)
 
 coutvectorize = CountVectorizer()
-token_vector = coutvectorize.fit_transform(token_list)
+token_vector = coutvectorize.fit_transform(token_process)
+
 
 w2v = Word2Vec(sentences= token_list, min_count= 1)
-token_word = w2v
 
-token_sent = lesk(token_list[0],"paris")
+
+token_sent = lesk(token_process[0],"paris")
 
 nlp = spacy.load("en_core_web_sm")
 token_sent2 =nlp("".join(token_list[0]))
 for token in token_sent2:
     print("{0}".format(token.head.text))
-
-rf = RandomForestClassifier()
-
 
 parameters = {'max_features': ('auto','sqrt'),
              'n_estimators': [500, 1000, 1500],
@@ -51,20 +66,10 @@ parameters = {'max_features': ('auto','sqrt'),
              'min_samples_leaf': [1, 2, 5, 10],
              'bootstrap': [True, False]}
 
-search = GridSearchCV(rf,parameters,cv=5,scoring='accuracy')
+search = GridSearchCV(RandomForestClassifier(),parameters,cv=5,scoring='accuracy')
 
 search.fit(token_vector)
 print(search.best_params_)
-
-
-
-
-
-
-
-
-
-
 
 
 
